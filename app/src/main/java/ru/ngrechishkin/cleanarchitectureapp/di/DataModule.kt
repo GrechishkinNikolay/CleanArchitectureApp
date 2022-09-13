a1f8e6256b2879b696ec0165596c3b9d8f74f30a
GrechishkinNikolay <kolaygreha@rambler.ru>
2022-09-13 14:46:53 +0300

    Clean Architecture + MVVM + LiveData + Hilt -  @HiltAndroidApp, @HiltViewModel, @EntryPoint

--- app/src/main/java/ru/ngrechishkin/cleanarchitectureapp/di/DataModule.kt
+++ app/src/main/java/ru/ngrechishkin/cleanarchitectureapp/di/DataModule.kt
@@ -3,21 +3,27 @@ package ru.ngrechishkin.cleanarchitectureapp.di
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import ru.ngrechishkin.cleanarchitectureapp.data.repository.UserRepositoryImpl
 import ru.ngrechishkin.cleanarchitectureapp.data.storage.IUserStorageDAO
 import ru.ngrechishkin.cleanarchitectureapp.data.storage.sharedPrefs.SharedPrefUserDAO
 import ru.ngrechishkin.cleanarchitectureapp.domain.repository.IUserRepository
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class DataModule {
 
     @Provides
+    @Singleton
     fun provideUserStorage(@ApplicationContext context: Context): IUserStorageDAO {
         return SharedPrefUserDAO(context = context)
     }
 
     @Provides
+    @Singleton
     fun provideUserRepository(userStorage: IUserStorageDAO): IUserRepository {
         return UserRepositoryImpl(userStorage = userStorage)
     }
--- app/src/main/java/ru/ngrechishkin/cleanarchitectureapp/di/DomainModule.kt
+++ app/src/main/java/ru/ngrechishkin/cleanarchitectureapp/di/DomainModule.kt
@@ -2,11 +2,14 @@ package ru.ngrechishkin.cleanarchitectureapp.di
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import ru.ngrechishkin.cleanarchitectureapp.domain.repository.IUserRepository
 import ru.ngrechishkin.cleanarchitectureapp.domain.usecase.GetUserNameUseCase
 import ru.ngrechishkin.cleanarchitectureapp.domain.usecase.SaveUserNameUseCase
 
 @Module
+@InstallIn(ViewModelComponent::class)
 class DomainModule {
 
     @Provides
